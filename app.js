b3830bed8ef775e46588ddf9e12eff70376a0a96
TheFlash180 <rickust18@gmail.com>
2020-11-03 11:20:59 +0200

    yetanother

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const { json } = require('express');
-
+/*
 const url = 'mongodb://test:testing123@54.164.251.124/Testing?retryWrites=true&w=majority';  //'mongodb+srv://Binero:Alfadelta4@cluster0mongodb.nwadf.mongodb.net/sample_airbnb?retryWrites=true&w=majority';//'mongodb://myUserAdmin:abc123@3.84.45.74/Admin?retryWrites=true&w=majority';
 
 
@@ -25,7 +25,8 @@ app.get('/', (req, res) => {
             console.log(result.name);             
     });
 });
-});
+});*/
+
 app.listen(PORT, () => {
     console.log("Server is running...");
 
